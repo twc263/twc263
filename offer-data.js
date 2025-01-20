bf5bd4066e375b99e6b8940b8337603ad335d294
twc263 <vuhuyinau@gmail.com>
2025-01-21 00:03:48 +1100

    Add files via upload

--- offer-data.js
+++ offer-data.js
@@ -215,3 +215,43 @@ const products = [
 }
 
 ];
+///load Products from offer data
+let offerProductHTML = '';
+
+products.forEach(product => {
+    offerProductHTML += `
+    <div class="product-offer-card">
+      <div class="product-container">
+        <div class="image-offer-wrapper">
+            <img loading="lazy" src="${product.imgSrc}"  alt="${product.name}" />
+            <div class="second-rotation"></div>
+        </div>
+        <div class="product-info">
+            <p class="producer-name">${product.producer}</p>
+            <p class="product-name">${product.name}</p>
+            <p class="product-type">${product.type}</p>
+            <p class="product-specs">${product.abv} <span class="small-bold-text">ABV</span>
+                , ${product.volume}<span class="small-bold-text"> ml</span></p>
+        </div>
+        <div class="product-pricing">
+            <p class="producer-location">${product.location}</p>
+            <p class="price-first"><span class="dollar-text">$ </span>${product.price1} <span class="quantity">EACH</span> </p>
+            <p class="price-second">${product.bundleQuantity}<span class="quantity"> FOR </span><span class="dollar-text">$ </span>${product.price2} </p>
+            <p class="rrp">RRP: ${product.rrp}</p>
+        </div>
+      </div>
+       <div class="quote-container">
+                <div class="quote-text">
+                    "${product.quote} <a href="${product.ratingLink}" class="more-link" target="_blank">... ${product.rating}</a><span>"</span>
+                </div>
+                <div class="order-button">
+                    <a href="${product.orderLink}" target="_blank">
+                        <button class="order-now">Claim Offer</button>
+                    </a>
+                </div>
+            </div>
+    </div>`;
+});
+
+  document.querySelector('.js-load-products').
+  innerHTML = offerProductHTML;
