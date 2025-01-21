01c7dc85435e6fd22edd1169b832cc686e83b595
twc263 <vuhuyinau@gmail.com>
2025-01-21 18:05:08 +1100

    Add files via upload

--- offer-data.js
+++ offer-data.js
@@ -1,257 +1,197 @@
 const products = [
-  {
-      producer: "Hawkers Beer",
-      name: "Witching Hour",
-      type: "Imperial Porter",
-      abv: "9%",
-      volume: 440,
-      location: "Reservoir",
-      price1: "12.99",
-      price2: "10.99",
-      rrp: "$14",
-      imgSrc: "./images/Witching Hour.svg",
-      quote: "Don't fear the darkest hour of night",
-      ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
+    {
+        producer: "Slow Lane",
+        name: "Classis Haze",
+        type: "Hefeweizen",
+        abv: "5.6%",
+        volume: 375,
+        location: "NSW",
+        price1: "5.49",
+        price2: "19.99",
+        rrp: "$6",
+        imgSrc: "./images/Classis Haze.jpg",
+        quote: "Hefeweizens are a traditional German wheat ale with th",
+        ratingLink: "https://untappd.com/b/slow-lane-brewing-classic-haze/4289840",
+        orderLink: "https://www.instagram.com/direct/t/17851876955786176",
+        rating: "3.66 / 5",
+        bundleQuantity: "4"
+    },
+    {
+        producer: "Slow Lane",
+        name: "Conditioned",
+        type: "Hazy Pale Ale",
+        abv: "5.4%",
+        volume: 375,
+        location: "NSW",
+        price1: "6.49",
+        price2: "23.99",
+        rrp: "$7",
+        imgSrc: "./images/Conditioned.jpg",
+        quote: "A combination of our favourite NZ and US hops",
+        ratingLink: "https://untappd.com/b/slow-lane-brewing-conditioned/5433235",
+        orderLink: "https://www.instagram.com/direct/t/17851876955786176",
+        rating: "3.79 / 5",
+        bundleQuantity: "4"
+  
+    },
+    {
+        producer: "Bacchus",
+        name: "Hyperspatial",
+        type: "Coffee Stout",
+        abv: "7.8%",
+        volume: 375,
+        location: "Queensland",
+        price1: "11.99",
+        price2: "22.99",
+        rrp: "$13",
+        imgSrc: "./images/Hyperspatial.jpg",
+        quote: "Made with the finest coffee beans, for a true Espresso experience",
+        ratingLink: "https://untappd.com/b/bacchus-brewing-co-hyperspatial-espresso-stout/1793124",
+        orderLink: "https://www.instagram.com/direct/t/17851876955786176",
+        rating: "4.04 / 5",
+        bundleQuantity: "2"
+    },
+    {
+      producer: "Bacchus",
+      name: "Hyperdelic",
+      type: "Hazy IPA",
+      abv: "7.4%",
+      volume: 375,
+      location: "Queensland",
+      price1: "11.99",
+      price2: "22.99",
+      rrp: "$13",
+      imgSrc: "./images/Hyperdelic.jpg",
+      quote: "A brand-new Hazy IPA showcasing 2 of the latest hype brewing in",
+      ratingLink: "https://untappd.com/b/bacchus-brewing-co-hyperdelic/6035227",
       orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "4.01 / 5",
+      rating: "4.15 / 5",
       bundleQuantity: "2"
   },
   {
-      producer: "Hawkers Beer",
-      name: "Bourbon Barrel Aged",
-      type: "White Stout (2024)",
-      abv: "6%",
+      producer: "Bojak",
+      name: "A Complex Juggle",
+      type: "Hazy IPA",
+      abv: "7.0%",
       volume: 440,
-      location: "Reservoir",
+      location: "Dandenong",
       price1: "10.99",
-      price2: "9.49",
+      price2: "20.99",
       rrp: "$12",
-      imgSrc: "./images/bba.svg",
-      quote: "There cannot be darkness without the light",
-      ratingLink: "https://untappd.com/b/hawkers-beer-bourbon-barrel-aged-white-stout-2024/5873622",
+      imgSrc: "./images/A Complex Juggle.jpg",
+      quote: "A delicious summer fruit combo of passionfruit, peach and ",
+      ratingLink: "https://untappd.com/b/bojak-brewing-a-complex-juggle/6122699",
       orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "3.82 / 5",
+      rating: "",
       bundleQuantity: "2"
-
   },
   {
-      producer: "Wolf of The Willow",
-      name: "Lark Barrel Aged",
-      type: "Imperial JSP V",
-      abv: "12.4%",
-      volume: 440,
-      location: "Mordialloc",
-      price1: "19.99",
-      price2: "17.99",
-      rrp: "$22",
-      imgSrc: "./images/Wolf of The Willow.svg",
-      quote: "An Imperial Smoke Porter rich with wh",
-      ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
+      producer: "Bojak",
+      name: "Fence Sitter",
+      type: "Oatcream Pale Ale",
+      abv: "5.7%",
+      volume: 375,
+      location: "Dandenong",
+      price1: "6.49",
+      price2: "23.99",
+      rrp: "$7",
+      imgSrc: "./images/Fence Sitter.jpg",
+      quote: "Light straw in colour with a decent splash of lactose leaving a sof ",
+      ratingLink: "https://untappd.com/b/bojak-brewing-fence-sitter/6066113",
       orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "4.29 / 5",
-      bundleQuantity: "2"
-
-  },
-  {
-      producer: "Sailors Grave ",
-      name: "Zserbó",
-      type: "Festive Stout",
-      abv: "6%",
-      volume: 355,
-      location: "Orbost",
-      price1: "7.99",
-      price2: "6.99",
-      rrp: "$9",
-      imgSrc: "./images/Zserbó.svg",
-      quote: "Based on the popular Hungarian de",
-      ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
-      orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "Only 1 left",
+      rating: "3.93 / 5",
       bundleQuantity: "4"
-
   },
   {
-      producer: "Bright Brewery",
-      name: "Leaf Peeper",
-      type: "Red Rye IPA",
-      abv: "6.4%",
-      volume: 440,
+      producer: "Bojak",
+      name: "Velvet King",
+      type: "Oatmeal Stout",
+      abv: "5.2%",
+      volume: 375,
+      location: "Dandenong",
+      price1: "5.49",
+      price2: "19.99",
+      rrp: "$6",
+      imgSrc: "./images/Velvet King.jpg",
+      quote: "Roasty toasted oats bring this oatmeal stout to life ",
+      ratingLink: "https://untappd.com/b/bojak-brewing-velvet-king/3296584",
+      orderLink: "https://www.instagram.com/direct/t/17851876955786176",
+      rating: "3.73 / 5",
+      bundleQuantity: "4"
+  },
+  {
+      producer: "Bright",
+      name: "Staircase",
+      type: "Porter",
+      abv: "5.2%",
+      volume: 375,
       location: "Bright",
-      price1: "9.99",
-      price2: "8.75",
-      rrp: "$11",
-      imgSrc: "./images/lEAF.svg",
-      quote: "Like the autumn foliage that makes B!",
-      ratingLink: "https://untappd.com/b/bright-brewery-leaf-peeper-red-rye-ipa/5776316",
+      price1: "6.49",
+      price2: "23.99",
+      rrp: "$7",
+      imgSrc: "./images/Staircase.jpg",
+      quote: "Complex layered flavours of creamy espresso, bittersweet ",
+      ratingLink: "https://untappd.com/b/bright-brewery-staircase-porter/38050",
       orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "Only 3 left",
+      rating: "3.66 / 5",
       bundleQuantity: "4"
-
   },
   {
-      producer: "Deed Brewing",
-      name: "Darker Ritual",
-      type: "BBA Imperial Stout",
-      abv: "13%",
-      volume: 440,
-      location: "Glen Iris",
-      price1: "19.99",
-      price2: "17.99",
-      rrp: "$22",
-      imgSrc: "./images/Darker Ritual.svg",
-      quote: "As soon as we started sharing Dark Ritual at this y",
-      ratingLink: "https://untappd.com/b/deeds-brewing-darker-ritual/4926151",
+      producer: "Bright",
+      name: "Hellfire",
+      type: "Amber Ale",
+      abv: "5%",
+      volume: 375,
+      location: "Bright",
+      price1: "5.99",
+      price2: "21.99",
+      rrp: "$6.49",
+      imgSrc: "./images/Hellfire.jpg",
+      quote: "Its fiery colour inspired us to name this beer after Hellfire Gully, ",
+      ratingLink: "https://untappd.com/b/bright-brewery-hellfire-amber/19324",
       orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-      rating: "Only 2 left",
-      bundleQuantity: "2"
-
+      rating: "3.47 / 5",
+      bundleQuantity: "4"
   },
-  {
-    producer: "Hawkers Beer",
-    name: "Witching Hour",
-    type: "Imperial Porter",
-    abv: "9%",
-    volume: 440,
-    location: "Reservoir",
-    price1: "12.99",
-    price2: "10.99",
-    rrp: "$14",
-    imgSrc: "./images/Witching Hour.svg",
-    quote: "Don't fear the darkest hour of night",
-    ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "4.01 / 5",
-    bundleQuantity: "2"
-},
-{
-    producer: "Hawkers Beer",
-    name: "Bourbon Barrel Aged",
-    type: "White Stout (2024)",
-    abv: "6%",
-    volume: 440,
-    location: "Reservoir",
-    price1: "10.99",
-    price2: "9.49",
-    rrp: "$12",
-    imgSrc: "./images/bba.svg",
-    quote: "There cannot be darkness without the light",
-    ratingLink: "https://untappd.com/b/hawkers-beer-bourbon-barrel-aged-white-stout-2024/5873622",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "3.82 / 5",
-    bundleQuantity: "2"
-
-},
-{
-    producer: "Wolf of The Willow",
-    name: "Lark Barrel Aged",
-    type: "Imperial JSP V",
-    abv: "12.4%",
-    volume: 440,
-    location: "Mordialloc",
-    price1: "19.99",
-    price2: "17.99",
-    rrp: "$22",
-    imgSrc: "./images/Wolf of The Willow.svg",
-    quote: "An Imperial Smoke Porter rich with wh",
-    ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "4.29 / 5",
-    bundleQuantity: "2"
-
-},
-{
-    producer: "Sailors Grave ",
-    name: "Zserbó",
-    type: "Festive Stout",
-    abv: "6%",
-    volume: 355,
-    location: "Orbost",
-    price1: "7.99",
-    price2: "6.99",
-    rrp: "$9",
-    imgSrc: "./images/Zserbó.svg",
-    quote: "Based on the popular Hungarian de",
-    ratingLink: "https://untappd.com/b/hawkers-beer-witching-hour/5874361",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "Only 1 left",
-    bundleQuantity: "4"
-
-},
-{
-    producer: "Bright Brewery",
-    name: "Leaf Peeper",
-    type: "Red Rye IPA",
-    abv: "6.4%",
-    volume: 440,
-    location: "Bright",
-    price1: "9.99",
-    price2: "8.75",
-    rrp: "$11",
-    imgSrc: "./images/lEAF.svg",
-    quote: "Like the autumn foliage that makes B!",
-    ratingLink: "https://untappd.com/b/bright-brewery-leaf-peeper-red-rye-ipa/5776316",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "Only 3 left",
-    bundleQuantity: "4"
-
-},
-{
-    producer: "Deed Brewing",
-    name: "Darker Ritual",
-    type: "BBA Imperial Stout",
-    abv: "13%",
-    volume: 440,
-    location: "Glen Iris",
-    price1: "19.99",
-    price2: "17.99",
-    rrp: "$22",
-    imgSrc: "./images/Darker Ritual.svg",
-    quote: "As soon as we started sharing Dark Ritual at this y",
-    ratingLink: "https://untappd.com/b/deeds-brewing-darker-ritual/4926151",
-    orderLink: "https://www.instagram.com/direct/t/17851876955786176",
-    rating: "Only 2 left",
-    bundleQuantity: "2"
-
-}
-
-];
-///load Products from offer data
-let offerProductHTML = '';
-
-products.forEach(product => {
-    offerProductHTML += `
-    <div class="product-offer-card">
-      <div class="product-container">
-        <div class="image-offer-wrapper">
-            <img loading="lazy" src="${product.imgSrc}"  alt="${product.name}" />
-            <div class="second-rotation"></div>
+  ];
+  ///load Products from offer data
+  let offerProductHTML = '';
+  
+  products.forEach(product => {
+      offerProductHTML += `
+      <div class="product-offer-card">
+        <div class="product-container">
+          <div class="image-offer-wrapper">
+              <img loading="lazy" src="${product.imgSrc}"  alt="${product.name}" />
+              <div class="second-rotation"></div>
+          </div>
+          <div class="product-info">
+              <p class="producer-name">${product.producer}</p>
+              <p class="product-name">${product.name}</p>
+              <p class="product-type">${product.type}</p>
+              <p class="product-specs">${product.abv} <span class="small-bold-text">ABV</span>
+                  , ${product.volume}<span class="small-bold-text"> ml</span></p>
+          </div>
+          <div class="product-pricing">
+              <p class="producer-location">${product.location}</p>
+              <p class="price-first"><span class="dollar-text">$ </span>${product.price1} <span class="quantity">EACH</span> </p>
+              <p class="price-second">${product.bundleQuantity}<span class="quantity"> FOR </span><span class="dollar-text">$ </span>${product.price2} </p>
+              <p class="rrp">RRP: ${product.rrp}</p>
+          </div>
         </div>
-        <div class="product-info">
-            <p class="producer-name">${product.producer}</p>
-            <p class="product-name">${product.name}</p>
-            <p class="product-type">${product.type}</p>
-            <p class="product-specs">${product.abv} <span class="small-bold-text">ABV</span>
-                , ${product.volume}<span class="small-bold-text"> ml</span></p>
-        </div>
-        <div class="product-pricing">
-            <p class="producer-location">${product.location}</p>
-            <p class="price-first"><span class="dollar-text">$ </span>${product.price1} <span class="quantity">EACH</span> </p>
-            <p class="price-second">${product.bundleQuantity}<span class="quantity"> FOR </span><span class="dollar-text">$ </span>${product.price2} </p>
-            <p class="rrp">RRP: ${product.rrp}</p>
-        </div>
-      </div>
-       <div class="quote-container">
-                <div class="quote-text">
-                    "${product.quote} <a href="${product.ratingLink}" class="more-link" target="_blank">... ${product.rating}</a><span>"</span>
-                </div>
-                <div class="order-button">
-                    <a href="${product.orderLink}" target="_blank">
-                        <button class="order-now">Claim Offer</button>
-                    </a>
-                </div>
-            </div>
-    </div>`;
-});
-
-  document.querySelector('.js-load-products').
-  innerHTML = offerProductHTML;
+         <div class="quote-container">
+                  <div class="quote-text">
+                      "${product.quote} <a href="${product.ratingLink}" class="more-link" target="_blank">... ${product.rating}</a><span>"</span>
+                  </div>
+                  <div class="order-button">
+                      <a href="${product.orderLink}" target="_blank">
+                          <button class="order-now">GET NOW</button>
+                      </a>
+                  </div>
+              </div>
+      </div>`;
+  });
+  
+    document.querySelector('.js-load-products').
+    innerHTML = offerProductHTML;
+  
